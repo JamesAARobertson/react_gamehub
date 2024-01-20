ce8b8d8f7927d6af8d8ff609c4c54fcaf24dddbe
James Robertson <jaarobertson92@gmail.com>
2024-01-20 17:13:08 +0000

    passing children

--- react-gamehub/src/App.tsx
+++ react-gamehub/src/App.tsx
@@ -1,15 +1,10 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["London", "New York", "Paris", "Tokyo", "San Francisco"];
-
-const handleSelectItem = (item: string) =>{
-console.log(item)
-}
-
+  
   return (
     <div>
-      <ListGroup items={items} heading={"Cities"} onSelectItem={handleSelectItem}/>
+      <Alert>Hello <span>World</span></Alert>
     </div>
   );
 }
