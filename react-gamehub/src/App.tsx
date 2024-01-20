20702eacd4d0a4f160b14b42b50c4cb821541481
James Robertson <jaarobertson92@gmail.com>
2024-01-20 17:58:00 +0000

    messing around with classes and bootstrap buttons. Getting used to interface

--- react-gamehub/src/App.tsx
+++ react-gamehub/src/App.tsx
@@ -1,10 +1,10 @@
-import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
   
   return (
     <div>
-      <Alert>Hello <span>World</span></Alert>
+      <Button onClick={() => console.log('Clicked')}>Press Me</Button>
     </div>
   );
 }
