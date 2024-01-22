992c7080dc4782dc3825fd0bcd32e3a1853e5e1e
James Robertson <jaarobertson92@gmail.com>
2024-01-22 20:42:54 +0000

    practice passing functions via props

--- react-gamehub/src/App.tsx
+++ react-gamehub/src/App.tsx
@@ -1,14 +1,23 @@
-import Button from "./components/Button";
-import Alert from "./components/Alert";
-import { useState } from "react";
+// import Button from "./components/Button";
+// import Alert from "./components/Alert";
+// import { useState } from "react";
+import ListGroup from "./components/ListGroup/ListGroup";
 
 function App() {
-  const [alertVisible, setAlertVisible] = useState(false) 
+  // const [alertVisible, setAlertVisible] = useState(false)
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const heading = "Cities";
+
+  const handleSelectItem = (item: string) => {
+    console.log(item)
+  }
+
 
   return (
     <div>
-      { alertVisible && <Alert onClose={() => setAlertVisible(false)}>My Alert</Alert>}
-      <Button color="primary" onClick={() => setAlertVisible(true)}>Press Me</Button>
+      <ListGroup items={items} heading={heading} onSelectItem={handleSelectItem}></ListGroup>
+      {/* { alertVisible && <Alert onClose={() => setAlertVisible(false)}>My Alert</Alert>}
+      <Button color="primary" onClick={() => setAlertVisible(true)}>Press Me</Button> */}
     </div>
   );
 }
