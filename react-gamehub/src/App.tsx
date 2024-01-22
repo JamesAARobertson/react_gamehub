d3fd1a5d39a5b97e2aba5afbbaa8d1e0e1abf40c
James Robertson <jaarobertson92@gmail.com>
2024-01-22 19:40:56 +0000

    showing an alert exercise

--- react-gamehub/src/App.tsx
+++ react-gamehub/src/App.tsx
@@ -1,10 +1,14 @@
 import Button from "./components/Button";
+import Alert from "./components/Alert";
+import { useState } from "react";
 
 function App() {
-  
+  const [alertVisible, setAlertVisible] = useState(false) 
+
   return (
     <div>
-      <Button onClick={() => console.log('Clicked')}>Press Me</Button>
+      { alertVisible && <Alert onClose={() => setAlertVisible(false)}>My Alert</Alert>}
+      <Button color="primary" onClick={() => setAlertVisible(true)}>Press Me</Button>
     </div>
   );
 }
