71031e2b1527f1e32575a7664ed10a31566adbae
James Robertson <jaarobertson92@gmail.com>
2024-01-23 20:19:52 +0000

    react icons styling practice

--- react-gamehub/src/App.tsx
+++ react-gamehub/src/App.tsx
@@ -1,23 +1,11 @@
-// import Button from "./components/Button";
-// import Alert from "./components/Alert";
-// import { useState } from "react";
-import ListGroup from "./components/ListGroup";
+import {BsFillCalendarFill } from 'react-icons/bs'
 
 function App() {
-  // const [alertVisible, setAlertVisible] = useState(false)
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  const heading = "Cities";
-
-  const handleSelectItem = (item: string) => {
-    console.log(item)
-  }
 
 
   return (
     <div>
-      <ListGroup items={items} heading={heading} onSelectItem={handleSelectItem}></ListGroup>
-      {/* { alertVisible && <Alert onClose={() => setAlertVisible(false)}>My Alert</Alert>}
-      <Button color="primary" onClick={() => setAlertVisible(true)}>Press Me</Button> */}
+      <BsFillCalendarFill color="red" size="40" />
     </div>
   );
 }
--- react-gamehub/src/components/ListGroup/ListGroup.tsx
+++ react-gamehub/src/components/ListGroup/ListGroup.tsx
@@ -1,20 +1,20 @@
 import { useState } from "react";
 import "./ListGroup.css";
-import styled from "styled-components";
+// import styled from "styled-components";
 
-const List = styled.ul`
-  list-style: none;
-  padding: 0;
-`;
+// const List = styled.ul`
+//   list-style: none;
+//   padding: 0;
+// `;
 
-interface ListItemProps {
-  active: boolean
-}
+// interface ListItemProps {
+//   active: boolean
+// }
 
-const ListItem = styled.li<ListItemProps>`
-  padding: 5px 0;
-  background: ${(props) => (props.active ? "blue" : "none")}
-`;
+// const ListItem = styled.li<ListItemProps>`
+//   padding: 5px 0;
+//   background: ${(props) => (props.active ? "blue" : "none")}
+// `;
 
 interface Props {
   items: string[];
@@ -23,16 +23,16 @@ interface Props {
 }
 
 function ListGroup({ items, heading, onSelectItem }: Props) {
-  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>{heading}</h1>
       {items.length === 0 && <p>No item found.</p>}
-      <List>
+      <ul className="list-group" style={{ backgroundColor: 'yellow'}}>
         {items.map((item, index) => (
-          <ListItem
-          active={index === selectedIndex}
+          <li
+          // active={index === selectedIndex}
             key={item}
             onClick={() => {
               setSelectedIndex(index);
@@ -40,9 +40,9 @@ function ListGroup({ items, heading, onSelectItem }: Props) {
             }}
           >
             {item}
-          </ListItem>
+          </li>
         ))}
-      </List>
+      </ul>
     </>
   );
 }
