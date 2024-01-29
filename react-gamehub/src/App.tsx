5abb3ff74a69c8c2517f1035dd95a89a481c5303
James Robertson <jaarobertson92@gmail.com>
2024-01-29 19:24:51 +0000

    sharing state practice

--- react-gamehub/src/App.tsx
+++ react-gamehub/src/App.tsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
+import NavBar from "./components/NavBar";
+import Cart from "./components/Cart";
 
 
 function App() {
   const [cartItems, setCartItems] = useState(['Product1', 'Product2'])
   
-  const handleClick = () => {
-
 
 
   return (
     <div>
+      <NavBar cartItemsCount={cartItems.length}/>
+      <Cart cartItems = {cartItems} onClear={() => setCartItems([])}/>
     </div>
   );
 }
