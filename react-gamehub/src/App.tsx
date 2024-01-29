0ffdbfcc880556bc81806b4ef6f67f0d839a3ec3
James Robertson <jaarobertson92@gmail.com>
2024-01-29 19:51:16 +0000

    exercise 3

--- react-gamehub/src/App.tsx
+++ react-gamehub/src/App.tsx
@@ -1,17 +1,22 @@
 import { useState } from "react";
 
-
 function App() {
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepperoni",
-    toppings: ["Mushroom"]
-    },
-  )
-  
-const handleClick = () => {
-  setPizza({...pizza, toppings: [...pizza.toppings, "Cheese"] })
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
+  });
 
-}
+  const handleClick = () => {
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: 2 } : item
+      ),
+    });
+  };
 
   return (
     <div>
