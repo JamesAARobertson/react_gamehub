80f3264c06ec139561b4a466463b53194be4cbe6
James Robertson <jaarobertson92@gmail.com>
2024-02-19 18:48:26 +0000

    updating state practice 1

--- react-gamehub/src/App.tsx
+++ react-gamehub/src/App.tsx
@@ -1,21 +1,16 @@
 import { useState } from "react";
 
 function App() {
-  const [cart, setCart] = useState({
-    discount: 0.1,
-    items: [
-      { id: 1, title: "Product 1", quantity: 1 },
-      { id: 2, title: "Product 2", quantity: 1 },
-    ],
+  const [game, setGame] = useState({
+    id: 1,
+    player: {
+      name: "John",
+    },
   });
 
+
   const handleClick = () => {
-    setCart({
-      ...cart,
-      items: cart.items.map((item) =>
-        item.id === 1 ? { ...item, quantity: 2 } : item
-      ),
-    });
+    setGame({...game, player: { ...game.player, name: "Psyduck" }})
   };
 
   return (
