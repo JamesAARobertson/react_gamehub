c104665c1671165a13b0629c996e9404df588229
James Robertson <jaarobertson92@gmail.com>
2024-02-21 17:57:09 +0000

    applying validation

--- react-gamehub/src/components/Form.tsx
+++ react-gamehub/src/components/Form.tsx
@@ -1,31 +1,45 @@
 import React, { FormEvent, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 
+interface FormData {
+  name: string;
+  age: number;
+}
+
 const Form = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
 
   const onSubmit = (data: FieldValues) => console.log(data);
 
-
   return (
-    <form onSubmit={handleSubmit(data => console.log(data))}>
+    <form onSubmit={handleSubmit((data) => console.log(data))}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
         <input
-          { ...register("name")}
+          {...register("name", { required: true, minLength: 3 })}
           id="name"
           type="text"
           className="form-control"
         ></input>
+        {errors.name?.type === "required" && (
+          <p className="text-danger">The name field is required.</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger">The name must be at least 3 characters</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
         <input
-          { ...register("age")}
+          {...register("age")}
           id="age"
           type="number"
           className="form-control"
