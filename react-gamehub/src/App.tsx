d96fbc6ac2c2f4608fb1e030a3038a9aaa304964
James Robertson <jaarobertson92@gmail.com>
2024-02-23 18:57:48 +0000

    building an expense form. Really getting the hang of passing props :). Used javascript table elements

--- react-gamehub/src/App.tsx
+++ react-gamehub/src/App.tsx
@@ -1,9 +1,15 @@
-import  Form  from "./components/Form.tsx"
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
+  const expenses = [
+    {id: 1, description: "Milk", amount: 1.50, category: "Groceries"},
+    {id: 2, description: "Brain", amount: 150, category: "Utility"},
+    {id: 3, description: "Witches", amount: 20, category: "Occult"},
+    {id: 4, description: "Dark Souls 2", amount: 50, category: "Recreation"}
+  ]
   return (
     <div>
-     <Form />
+     <ExpenseList expenses={expenses} onDelete={(id) => console.log("Deleted", id)}></ExpenseList>
     </div>
   );
 }
