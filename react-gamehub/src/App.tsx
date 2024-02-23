f30b2b276c9579c97f2a42fd087c95295d9c8444
James Robertson <jaarobertson92@gmail.com>
2024-02-23 20:48:34 +0000

    delete expense functionality added

--- react-gamehub/src/App.tsx
+++ react-gamehub/src/App.tsx
@@ -1,15 +1,19 @@
+import { useState } from "react";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
-  const expenses = [
-    {id: 1, description: "Milk", amount: 1.50, category: "Groceries"},
-    {id: 2, description: "Brain", amount: 150, category: "Utility"},
-    {id: 3, description: "Witches", amount: 20, category: "Occult"},
-    {id: 4, description: "Dark Souls 2", amount: 50, category: "Recreation"}
-  ]
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "Milk", amount: 1.5, category: "Groceries" },
+    { id: 2, description: "Brain", amount: 150, category: "Utility" },
+    { id: 3, description: "Witches", amount: 20, category: "Occult" },
+    { id: 4, description: "Dark Souls 2", amount: 50, category: "Recreation" },
+  ]);
   return (
     <div>
-     <ExpenseList expenses={expenses} onDelete={(id) => console.log("Deleted", id)}></ExpenseList>
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter(e => e.id !== id))}
+      ></ExpenseList>
     </div>
   );
 }
