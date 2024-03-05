ef4af90f4f9c4bd1428ff9b865e0908f1afd80e7
James Robertson <jaarobertson92@gmail.com>
2024-03-05 13:14:58 +0000

    building the expense form

--- react-gamehub/src/expense-tracker/components/ExpenseFilter.tsx
+++ react-gamehub/src/expense-tracker/components/ExpenseFilter.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { categories } from "../../App";
 
 interface Props {
     onSelectCategory: (catergory: string) => void;
@@ -8,9 +8,7 @@ const ExpenseFilter = ({ onSelectCategory }: Props) => {
   return (
     <select className="form-select" onChange={(event) => onSelectCategory(event.target.value)}>
         <option value="">All categories</option>
-        <option value="Groceries">Groceries</option>
-        <option value="Utility">Utility</option>
-        <option value="Entertainment">Entertainment</option>
+        {categories.map(category => <option key={category} value={category}>{category}</option>)}
     </select>
   )
 }
