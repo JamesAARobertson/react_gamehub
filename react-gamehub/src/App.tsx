5248a8649635d80203e8a15d53326ec0e682af93
James Robertson <jaarobertson92@gmail.com>
2024-03-05 12:55:52 +0000

    building expense filter

--- react-gamehub/src/App.tsx
+++ react-gamehub/src/App.tsx
@@ -1,18 +1,35 @@
 import { useState } from "react";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/ExpenseFilter";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
   const [expenses, setExpenses] = useState([
     { id: 1, description: "Milk", amount: 1.5, category: "Groceries" },
     { id: 2, description: "Brain", amount: 150, category: "Utility" },
-    { id: 3, description: "Witches", amount: 20, category: "Occult" },
-    { id: 4, description: "Dark Souls 2", amount: 50, category: "Recreation" },
+    { id: 3, description: "Witches", amount: 20, category: "Entertainment" },
+    {
+      id: 4,
+      description: "Dark Souls 2",
+      amount: 50,
+      category: "Entertainment",
+    },
   ]);
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
+
   return (
     <div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
       <ExpenseList
-        expenses={expenses}
-        onDelete={(id) => setExpenses(expenses.filter(e => e.id !== id))}
+        expenses={visibleExpenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
       ></ExpenseList>
     </div>
   );
