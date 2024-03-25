5f738f1d46540062f72aff3c0d623dbd4c098baf
James Robertson <jaarobertson92@gmail.com>
2024-03-25 15:48:44 +0000

    Using the Effect Hook

--- react-gamehub/src/App.tsx
+++ react-gamehub/src/App.tsx
@@ -1,42 +1,21 @@
-import { useState } from "react";
-import ExpenseList from "./expense-tracker/components/ExpenseList";
-import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-import categories from "./expense-tracker/categories";
-
+import { useEffect, useRef, useState } from "react";
 
 function App() {
-  const [selectedCategory, setSelectedCategory] = useState("");
-  const [expenses, setExpenses] = useState([
-    { id: 1, description: "Milk", amount: 1.5, category: "Groceries" },
-    { id: 2, description: "Brain", amount: 150, category: "Utility" },
-    { id: 3, description: "Witches", amount: 20, category: "Entertainment" },
-    {
-      id: 4,
-      description: "Dark Souls 2",
-      amount: 50,
-      category: "Entertainment",
-    },
-  ]);
+  const ref = useRef<HTMLInputElement>(null);
 
-  const visibleExpenses = selectedCategory
-    ? expenses.filter((e) => e.category === selectedCategory)
-    : expenses;
+  //  After Render
+  useEffect(() => {
+    //  Side Effect
+    if (ref.current) ref.current.focus();
+  });
+
+  useEffect(() => {
+    document.title ="My App";
+  })
 
   return (
     <div>
-      <div className="mb-5">
-        <ExpenseForm onSubmit={expense => setExpenses([...expenses, {...expense, id: expenses.length + 1}])}/>
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter
-          onSelectCategory={(category) => setSelectedCategory(category)}
-        />
-      </div>
-      <ExpenseList
-        expenses={visibleExpenses}
-        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
-      ></ExpenseList>
+      <input ref={ref} type="text" className="form-control" />
     </div>
   );
 }
