d92f2f497efb1273352f327ab3202df46fc02cd6
James Robertson <jaarobertson92@gmail.com>
2024-03-25 16:20:00 +0000

    effect dependencies

--- react-gamehub/src/App.tsx
+++ react-gamehub/src/App.tsx
@@ -1,21 +1,18 @@
 import { useEffect, useRef, useState } from "react";
+import ProductList from "./components/ProductList";
 
 function App() {
-  const ref = useRef<HTMLInputElement>(null);
+  const [category, setCategory] = useState("")
 
-  //  After Render
-  useEffect(() => {
-    //  Side Effect
-    if (ref.current) ref.current.focus();
-  });
-
-  useEffect(() => {
-    document.title ="My App";
-  })
 
   return (
     <div>
-      <input ref={ref} type="text" className="form-control" />
+      <select className="form-select" onChange={(event) => setCategory(event.target.value)}>
+      <option value=""></option>
+      <option value="Clothing">Clothing</option>
+      <option value="Household">Household</option>
+      </select>
+      <ProductList category={category}/>
     </div>
   );
 }
