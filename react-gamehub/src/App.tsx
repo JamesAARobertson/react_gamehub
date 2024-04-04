54dca2735b1f6f26b7fc31b4df7ecddbe035a240
James Robertson <jaarobertson92@gmail.com>
2024-04-04 19:46:23 +0100

    deleting users

--- react-gamehub/src/App.tsx
+++ react-gamehub/src/App.tsx
@@ -27,24 +27,41 @@ function App() {
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
-        setIsLoading(false)
-      })
-      // Doesn't seem to work in Strict Mode
-      // .finally(() => {
-      //   setIsLoading(false)
-      // });
+        setIsLoading(false);
+      });
+    // Doesn't seem to work in Strict Mode
+    // .finally(() => {
+    //   setIsLoading(false)
+    // });
 
     return () => controller.abort();
   }, []);
+
+  const deleteUser =(user: User) => {
+    const originalUsers =[...users]
+    setUsers(users.filter(u => u.id !== user.id))
+
+    axios.delete(`https://jsonplaceholder.typicode.com/users/${user.id}`)
+    .catch(err => {
+      setError(err.message)
+      setUsers(originalUsers)
+    })
+  }
 
   return (
     <>
       <div>
         {error && <p className="text-danger">{error}</p>}
-        { isLoading && <div className="spinner-border"></div>}
-        <ul>
+        {isLoading && <div className="spinner-border"></div>}
+        <ul className="list-group">
           {users.map((user) => (
-            <li key={user.id}>{user.name}</li>
+            <li
+              key={user.id}
+              className="list-group-item d-flex justify-content-between"
+            >
+              {user.name}
+              <button className="btn btn-outline-danger" onClick={() => deleteUser(user)}>Delete</button>
+            </li>
           ))}
         </ul>
       </div>
