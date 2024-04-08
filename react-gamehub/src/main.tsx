02365d284c3a24a238f4de0f92b4299842eefdfe
James Robertson <jaarobertson92@gmail.com>
2024-04-08 16:37:31 +0100

    installing and familiarising with chakraUI and cleaning out notes and exercises to build app

--- react-gamehub/src/main.tsx
+++ react-gamehub/src/main.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx';
-import 'bootstrap/dist/css/bootstrap.css';
-import './index.css'
+import * as React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import * as ReactDOM from 'react-dom/client'
+import App from './App'
 
-const domNode = document.getElementById('root');
-const root = createRoot(domNode);
-
-root.render(
+const rootElement = document.getElementById('root') as HTMLElement
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
-  // document.getElementById('root')
-);
+)
