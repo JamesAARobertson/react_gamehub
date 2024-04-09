21791c6df06a27763f9b9f77dda76ee17b10b42b
James Robertson <jaarobertson92@gmail.com>
2024-04-09 10:42:05 +0100

    fetched games and rendered as ul

--- react-gamehub/src/services/api-client.ts
+++ react-gamehub/src/services/api-client.ts
@@ -1,7 +1,8 @@
-import axios, { CanceledError } from "axios"
+import axios from "axios"
 
 export default axios.create({
-    baseURL: "https://jsonplaceholder.typicode.com"
+    baseURL: "https://api.rawg.io/api",
+   params: {
+    key: "b3bb917b32ec4a4282c4b49710f6476b"
+   }
 })
-
-export { CanceledError }
